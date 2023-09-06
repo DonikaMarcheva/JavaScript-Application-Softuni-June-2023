dd762a7ff4ff248d8723636bc441c650b7477d22
Donika <marchevadonika@gmail.com>
2023-09-06 22:30:50 +0300

    Create catalog.js

---
+++ examPreparation/JS Applications Retake Exam - 8 August 2023/src/views/catalog.js
@@ -0,0 +1,23 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { getAllMotorcycles } from "../data/motorcycles.js";
+
+const catalogTemplate = (motorcycles) => 
+html `<h2>Available Motorcycles</h2>
+<section id="dashboard">
+${motorcycles.length >0 ? motorcycles.map(motorcycleCard):html `<h2 class="no-avaliable">No avaliable motorcycles yet.</h2>`}
+</section>
+`
+const motorcycleCard=(motorcycle)=>html`
+  <div class="motorcycle">
+            <img src=${motorcycle.imageUrl} alt="example1" />
+            <h3 class="model">${motorcycle.model}</h3>
+            <p class="year">Year: ${motorcycle.year}</p>
+            <p class="mileage">Mileage: ${motorcycle.mileage} km.</p>
+            <p class="contact">Contact Number: ${motorcycle.contact}</p>
+            <a class="details-btn" href="/catalog/${motorcycle._id}">More Info</a>
+          </div>
+`
+export async function catalogPage(ctx) {
+    const motorcycles=await getAllMotorcycles()
+      ctx.render(catalogTemplate(motorcycles))
+  }
