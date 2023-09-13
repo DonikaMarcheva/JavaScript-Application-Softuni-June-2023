88022226c065d86db339733a057831b79571558d
Donika <marchevadonika@gmail.com>
2023-09-13 23:46:24 +0300

    Create api.js

---
+++ 04.Architecture and Testing - Exercise/03.SoftTerest/src/api/api.js
@@ -0,0 +1,48 @@
+const host='http://localhost:3030/'
+
+async function requester(method,url,data){
+    const user=JSON.parse(localStorage.getItem('user'));
+    
+    const option={
+        method,
+        headers:{}
+    };
+    if(data){
+        option.headers['Content-Type']='application/json';
+        option.body=JSON.stringify(data);
+    }
+    if(user){
+        const token=user.accessToken;
+        option.headers['X-Authorization']=token;
+    }
+    try{
+        const response=await fetch(host+url, option);
+       
+        if(!response.ok){
+            if(response.status===403){
+                localStorage.removeItem('user');
+            }
+            const err=await response.json();
+            throw new Error(err.message)
+        }
+        if(response.status===204){
+            return response;
+        }else{
+            return await response.json();
+        }
+    }catch(error){
+        alert(error.message);
+        throw error;
+    }
+   }
+const get=requester.bind(null,'get');
+const post=requester.bind(null,'post');
+const put=requester.bind(null,'put');
+const del=requester.bind(null,'delete');
+
+export {
+    get,
+    post,
+    put,
+    del as delete
+}
