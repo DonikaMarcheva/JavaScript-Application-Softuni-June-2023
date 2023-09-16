505c19260fda37af82ea1156f482bc90456bdcb1
Donika <marchevadonika@gmail.com>
2023-09-16 21:41:59 +0300

    Create app.js

---
+++ 05.Client Side Rendering - Exercise/01.List-Towns/app.js
@@ -0,0 +1,27 @@
+import {html,render} from '../node_modules/lit-html/lit-html.js'
+
+const form=document.querySelector('form');
+form.addEventListener('submit',onSubmit);
+const root=document.getElementById('root'); 
+function onSubmit(e){
+e.preventDefault();
+const formData = new FormData(form);
+const towns= formData.get('towns');
+const townsArr=towns.split(', ');
+renderTowns(townsArr);
+form.reset();
+} 
+
+function renderTowns(data){
+    const result=createTownsList(data);
+    render(result,root);
+}
+
+function createTownsList(data){
+  const ul = html `
+  <ul>
+    ${data.map(el => html `<li>${el}</li>`)}
+  </ul>
+  `
+  return ul;
+}
